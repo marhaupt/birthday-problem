2d946f0e8f622417f3b8402618b49a4a9957af94
Martin Haupt <martin23haupt@gmail.com>
2017-12-16 13:14:24 +0100

    prettier basic setup

--- src/App.js
+++ src/App.js
@@ -8,55 +8,63 @@ class App extends Component {
   handleInput = e => {
     const value = e.target.value;
 
-    const newValue = value < 1 ? 1: value;
+    const newValue = value < 1 ? 1 : value;
 
     this.setState({
       students: newValue
     });
-  }
+  };
 
   changeNumber = e => {
     const name = e.target.name;
 
-    const newNumber = this.state.students + (name === 'plus' ? 1 : -1)
+    const newNumber = this.state.students + (name === 'plus' ? 1 : -1);
 
     this.setState({
       students: newNumber
     });
-  }
+  };
 
   render() {
-    const {students} = this.state;
-    const result = percentaged(1-multi(366, students) / (366 ** students), 2);
+    const { students } = this.state;
+    const result = percentaged(1 - multi(366, students) / 366 ** students, 2);
 
     return (
       <div className="App">
         <h1>Birthday problem</h1>
         <h2>Number of people</h2>
         <p>
-          <button className="change-count" name="minus" onClick={this.changeNumber}>
+          <button
+            className="change-count"
+            name="minus"
+            onClick={this.changeNumber}
+          >
             -
           </button>
-          <input
-            type="number"
-            value={students}
-            onChange={this.handleInput}
-            />
-          <button className="change-count" name="plus" onClick={this.changeNumber}>+</button>
+          <input type="number" value={students} onChange={this.handleInput} />
+          <button
+            className="change-count"
+            name="plus"
+            onClick={this.changeNumber}
+          >
+            +
+          </button>
         </p>
-        <p className="result">{(students <= 366 && result === 100) && 'almost '}{ result }&nbsp;%</p>
-        <p><a href="https://en.wikipedia.org/wiki/Birthday_problem">Wiki</a></p>
+        <p className="result">
+          {students <= 366 && result === 100 && 'almost '}
+          {result}&nbsp;%
+        </p>
+        <p>
+          <a href="https://en.wikipedia.org/wiki/Birthday_problem">Wiki</a>
+        </p>
       </div>
     );
   }
 }
 
-const multi = (a, b) => (
-  (a === 366 - b || b >= 100) ? 1 : a * multi(a-1, b)
-)
+const multi = (a, b) => (a === 366 - b || b >= 100 ? 1 : a * multi(a - 1, b));
 
-const percentaged = (number, dec) => (
-  Math.round(100 * number * (10 ** dec)) / (10 ** dec)
-)
+const percentaged = (number, dec) =>
+  Math.round(100 * number * 10 ** dec) / 10 ** dec;
 
 export default App;
